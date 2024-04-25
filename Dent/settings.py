cda01a8bda872c48787830ac3c4a2fc4b984c8bd
DAMunene <muriukimoff6@gmail.com>
2024-04-25 12:28:23 +0300

    ignored some files

--- Dent/settings.py
+++ Dent/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', default='django-insecure-ee_)88s$sfxrx=ur9((lclst(+o-0yf09l=!%*()+ss+))*xe1')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =  'RENDER' not in os.environ
 
