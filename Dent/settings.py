3fb8858f368f6d5b1d83a6ee1bc138d3c3325ff0
DAMunene <muriukimoff6@gmail.com>
2024-04-25 13:48:49 +0300

    Added the dockerfile

--- Dent/settings.py
+++ Dent/settings.py
@@ -24,13 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  'RENDER' not in os.environ
+DEBUG =  os.environ.get("DEBUG", True)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".onrender.com"]
 
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
 
 # Application definition
 
